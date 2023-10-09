925429fa13638deb1ce506ea5b4345e130317bfa
Evelina Derezhenko <113761562+Ev1k@users.noreply.github.com>
2023-10-09 20:45:18 +0300

    preparing

---
+++ src/main/java/com/derezhenko/net/client/NetConnection.java
@@ -0,0 +1,26 @@
+package com.derezhenko.net.client;
+
+import java.util.Map;
+
+public class NetConnection implements HttpClient{
+
+    @Override
+    public String get(String url, Map<String, String> params) {
+        return null;
+    }
+
+    @Override
+    public String post(String url, Map<String, String> params) {
+        return null;
+    }
+
+    @Override
+    public String put(String url, Map<String, String> params) {
+        return null;
+    }
+
+    @Override
+    public String delete(String url, Map<String, String> params) {
+        return null;
+    }
+}
